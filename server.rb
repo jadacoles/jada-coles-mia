c416582685d3e8abc6be8d9605209f9819f83514
alexisdiaz008 <lexmasta@gmail.com>
2018-12-15 17:06:41 -0500

    added wcag

--- server.rb
+++ server.rb
@@ -1,6 +1,10 @@
  require 'sinatra'
 
- get '/' do
-
+get '/' do
  send_file File.expand_path('index.html', settings.public_folder)
 end
+
+get '/wcag' do
+ send_file File.expand_path('wcag.html', settings.public_folder)
+end
+
